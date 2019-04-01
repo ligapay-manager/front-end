82423416a6aa53793e53f6ded7fba336b1247cb0
Sávio Serra <serra.santos.s@gmail.com>
2019-04-01 10:29:39 -0300

    Redux and Redux-Thunk

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './src/App';
+import { Provider } from 'react-redux';
 import { name as appName } from './app.json';
 
+import store from './src/redux/store';
+import App from './src/App';
 
-AppRegistry.registerComponent(appName, () => App);
+
+const AppStart = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => AppStart);
---
+++ src/redux/reducers/appReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  example: 2
+};
+
+const appReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default appReducer;
