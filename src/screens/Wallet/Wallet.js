3df9bcb52489e7139b99aefa85699166376190d5
Sávio Serra <serra.santos.s@gmail.com>
2019-06-03 12:21:16 -0300

    Melhoria na interface de Wallet e Login

--- src/screens/Wallet/Wallet.js
+++ src/screens/Wallet/Wallet.js
@@ -1,32 +1,19 @@
 /* eslint-disable max-len */
 import React from 'react';
-import {
-  Text,
-  StatusBar,
-  ActivityIndicator,
-  FlatList,
-  Clipboard,
-  ToastAndroid,
-  TouchableNativeFeedback
-} from 'react-native';
+import { Text, Clipboard, ToastAndroid } from 'react-native';
 import { Query } from 'react-apollo';
 import { connect } from 'react-redux';
 import ActionButton from 'react-native-action-button';
 import QRCode from 'react-native-qrcode';
-import moment from 'moment';
-import 'moment/locale/pt-br';
-
 import Icon from 'react-native-vector-icons/FontAwesome5';
-
-import { AmountContainer, Container, IconStyle, QRCodeContainer, WalletView, TransactionContainer } from './components';
+import { Container, IconStyle, QRCodeContainer, WalletView, AmountText, CardsContainer, Card } from './components';
 
 import { WALLET } from '../../graphql/query/user';
 
 
 class Wallet extends React.Component {
   state = {
-    amount: null,
-    transactions: []
+    amount: null
   };
 
   onQrCodeClick = () => {
@@ -37,71 +24,45 @@ class Wallet extends React.Component {
   };
 
   onQueryCompleted = ({ me: { wallet } }) => {
-    const { amount, transactions } = wallet;
+    const { amount } = wallet;
 
-    this.setState({ amount, transactions });
+    this.setState({ amount });
   };
 
   render() {
-    const { amount, transactions } = this.state;
+    const { amount } = this.state;
     const { walletId, navigation } = this.props;
 
     return (
       <WalletView>
-        <StatusBar animated backgroundColor="#043927" />
+        <Container colors={['#14995D', '#168C57']}>
+          <QRCodeContainer onPress={this.onQrCodeClick}>
+            <QRCode value={walletId} size={130} bgColor="#14995D" fgColor="#fff" />
+          </QRCodeContainer>
 
-        <Query query={WALLET} onCompleted={this.onQueryCompleted} fetchPolicy="no-cache">
-          {({ loading }) => {
-            if (loading) {
-              return <ActivityIndicator animating color="#c6c013" />;
-            }
+          <AmountText>
+            <Query query={WALLET} fetchPolicy="no-cache" onCompleted={this.onQueryCompleted} pollInterval={30e3}>
+              {({ loading }) => (loading ? '...' : `R$ ${(amount / 100).toFixed(2)}`)}
+            </Query>
+          </AmountText>
+        </Container>
 
-            return (
-              <>
-                <Container>
-                  <QRCodeContainer onPress={this.onQrCodeClick}>
-                    <QRCode value={walletId} size={130} bgColor="#14995D" fgColor="#fff" />
-                  </QRCodeContainer>
+        <CardsContainer>
+          <Card>
+            <Icon name="credit-card" size={20} />
+            <Text style={{ color: 'grey' }}>DATA</Text>
+          </Card>
 
-                  <AmountContainer>
-                    <Icon name="money-bill" size={20} color="#4F7942" />
-                    <Text>{(amount / 100).toFixed(2)}</Text>
-                  </AmountContainer>
-                </Container>
+          <Card>
+            <Icon name="credit-card" size={20} />
+            <Text style={{ color: 'grey' }}>DATA</Text>
+          </Card>
 
-                {transactions && (
-                  <FlatList
-                    style={{
-                      backgroundColor: '#fff',
-                      borderRadius: 10,
-                      marginBottom: 10
-                    }}
-                    contentContainerStyle={{
-                      alignItems: 'center',
-                      marginBottom: 20
-                    }}
-                    data={transactions}
-                    keyExtractor={item => item.id}
-                    renderItem={({ item }) => (
-                      <TouchableNativeFeedback>
-                        <TransactionContainer>
-                          <Text style={{ color: 'grey' }}>{moment(item.createdAt).fromNow()}</Text>
-                          <Text
-                            style={{
-                              color: item.amount >= 0 ? 'black' : 'red'
-                            }}
-                          >
-                            {`R$ ${(item.amount / 100).toFixed(2)}`}
-                          </Text>
-                        </TransactionContainer>
-                      </TouchableNativeFeedback>
-                    )}
-                  />
-                )}
-              </>
-            );
-          }}
-        </Query>
+          <Card>
+            <Icon name="credit-card" size={20} />
+            <Text style={{ color: 'grey' }}>DATA</Text>
+          </Card>
+        </CardsContainer>
 
         <ActionButton buttonColor="#ffd300" hideShadow size={40} fixNativeFeedbackRadius zIndex={1}>
           <ActionButton.Item
--- src/screens/Wallet/components.js
+++ src/screens/Wallet/components.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-
+import LinearGradient from 'react-native-linear-gradient';
 import View from '../../components/View';
 
 
@@ -14,15 +14,16 @@ export const QRCodeContainer = styled.TouchableOpacity`
 `;
 
 export const WalletView = styled(View)`
-  background-color: #14995d;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
+  background-color: #fbfbfb;
 `;
 
-export const Container = styled.View`
+export const Container = styled(LinearGradient)`
   justify-content: center;
   align-items: center;
-  height: 50%;
+  width: 100%;
+  padding: 30px 0px;
 `;
 
 export const AmountContainer = styled.View`
@@ -36,6 +37,34 @@ export const AmountContainer = styled.View`
   border-radius: 10px;
   elevation: 4px;
   background-color: #fff;
+`;
+
+export const AmountText = styled.Text`
+  margin-top: 30px;
+  font-size: 30px;
+  color: #fff;
+  font-family: 'Abel-Regular';
+`;
+
+export const CardsContainer = styled.View`
+  flex: 2;
+  background-color: #fbfbfb;
+  padding: 20px;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+export const Card = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  height: 50px;
+  width: 90%;
+  elevation: 1px;
+  background-color: #fff;
+  border-radius: 4px;
 `;
 
 export const TransactionContainer = styled.View`
